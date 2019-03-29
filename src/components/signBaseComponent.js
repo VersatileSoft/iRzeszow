fdcee3a881c07586965b1baf2af8cb5ad4726147
LAPTOP-C4M3GNA0\Tomasz <tomkowa64@gmail.com>
2019-03-29 18:40:17 +0100

    The beggining of great desigining

--- src/components/signBaseComponent.js
+++ src/components/signBaseComponent.js
@@ -7,6 +7,34 @@ class SignBaseComponent extends Component {
     render() {
         return (
             <div>
+                <header>
+                    <div className="logo">
+                        iRzeszow
+                    </div>
+                    <div className="list">
+                        <div>1</div>
+                        <div>2</div>
+                        <div>3</div>
+                    </div>
+                </header>
+                <section className="main-square">
+                    <div>
+                        content
+                    </div>
+                </section>
+                <section className="lower-squares">
+                    <div className="middle-square">
+                        content
+                    </div>
+                    <div className="rest">
+                        <div className="adv">
+                        
+                        </div>
+                        <div className="smallest-squares">
+                        
+                        </div>
+                    </div>
+                </section>
             </div>
         )
     }
