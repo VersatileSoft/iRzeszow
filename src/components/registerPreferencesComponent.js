3d492d76d1a9e29a4ba5042a7c14be7b95e1b523
Marek K <wiedzmin1371@gmail.com>
2019-03-29 20:35:09 +0100

    Add fields for tests

--- src/components/registerPreferencesComponent.js
+++ src/components/registerPreferencesComponent.js
@@ -1,14 +1,39 @@
 import { Component } from 'react';
+import { Form } from 'final-form';
 
 import '../styles/signBaseComponent.scss';
+import { Form, Field } from 'react-final-form';
+import { TextField } from 'react-final-form-antd';
+
+import { Input } from 'antd';
+
+import CustomField from './CustomField';
 
 class RegisterPreferenesComponent extends Component {
 
+    handleChange() {
+        print("AA");
+    }
+
+    validateForm() {
+        print("BB");
+    }
+
     render() {
         return (
-            <div>
-            </div>
-        )
+            <From 
+                onSubmit={handleChange}
+                validate={validateForm}
+                render={({ handleSubmit, pristine, invalid }) => (
+                    <Field
+                        name={"Podaj nazwę"}
+                        component={TextField}
+                    >
+
+                    </Field>
+                )}
+            />
+        );
     }
 
 }
