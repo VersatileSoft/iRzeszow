ca8be844c97f98e60a2d9682e73d5f5798701147
Konrad Kolasa <rengar600@interia.pl>
2019-03-30 02:03:52 +0100

    css merge

--- src/components/registerUserComponent.js
+++ src/components/registerUserComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Form, Field } from 'react-final-form';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import '../styles/UniversalForm.scss';
 import { updateRegisterState } from '../actions/signUpActions/updateActions/updateRegisterState';
 import { updateUser } from '../actions/signUpActions/updateActions/updateUser';
 
@@ -42,61 +43,70 @@ class RegisterUserComponent extends Component {
                 onSubmit={this.submitForm}
                 validate={this.validateForm}
                 render={({ handleSubmit, pristine, invalid }) => (
-                    <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} className="form">
+                    <div className="input-wrapper">
                         <Field
-                            name="name"
-                            placeholder="Podaj imię"
-                            component="input"
-                            onInput={ e => {this.handleChange('name', e.target.value)}}
-                        />
-
+                                name="name"
+                                placeholder="Podaj nazwę"
+                                onChange={ e => {this.handleChange('name', e.target.value)}}
+                                render={({meta }) => (
+                                    <input type="text" id="user" name="name" required/>
+                                )}
+                            />
+                        <label htmlFor="user">Nazwa użytkownika</label>
+                    </div>
+                    <div className="input-wrapper">
                         <Field
-                            name="lastName"
-                            placeholder="Nazwisko"
-                            component="input"
-                            onInput={ e => {this.handleChange('surname', e.target.value)}}
-                        />
-
+                                name="surname"
+                                placeholder="Podaj nazwę"
+                                onChange={ e => {this.handleChange('surname', e.target.value)}}
+                                render={({meta }) => (
+                                    <input type="text" id="surname" name="surname" required/>
+                                )}
+                            />
+                        <label htmlFor="surname">Nazwa użytkownika</label>
+                    </div>
+                    <div className="input-wrapper">
                         <Field
-                            name="password"
-                            placeholder="Hasło"
-                            component="input"
-                            onInput={ e => {this.handleChange('password', e.target.value)}}
-                        />
+                                name="password"
+                                placeholder="Podaj nazwę"
+                                onChange={ e => {this.handleChange('password', e.target.value)}}
+                                render={({meta }) => (
+                                    <input type="text" id="password" name="password" required/>
+                                )}
+                            />
+                        <label htmlFor="password">Password</label>
+                    </div>
 
+                    <div className="input-wrapper">
                         <Field
-                            name="r_pass"
-                            placeholder="Powtórz hasło"
-                            component="input"
-                            onInput={ e => {this.handleRChange('r_pass', e.target.value)}}
-                        />
+                                name="r_pass"
+                                placeholder="Podaj nazwę"
+                                onChange={ e => {this.handleRChange('r_pass', e.target.value)}}
+                                render={({meta }) => (
+                                    <input type="text" id="r_pass" name="r_pass" required/>
+                                )}
+                            />
+                        <label htmlFor="r_pass">Powtórz hasło</label>
+                    </div>
 
+                    <div className="input-wrapper">
                         <Field
-                            name="email"
-                            component="input"
-                            placeholder="Email"
-                            onInput={ e => {this.handleChange('email', e.target.value)}}
-                        />
+                                name="email"
+                                placeholder="Podaj nazwę"
+                                onChange={ e => {this.handleChange('email', e.target.value)}}
+                                render={({meta }) => (
+                                    <input type="text" id="email" name="email" required/>
+                                )}
+                            />
+                        <label htmlFor="email">E-mail</label>
+                    </div>
 
-                        {/* <Field
-                            name="gender"
-                            component="input"
-                            placeholder="Płeć"
-                            onInput={ e => {this.handleChange('gender', e.target.value)}}
-                        /> */}
+                    <button type="submit">Zatwierdź</button>
 
-                        <Field
-                            name="phone"
-                            component="input"
-                            placeholder="Numer telefonu"
-                            onInput={ e => {this.handleChange('phone', e.target.value)}}
-                        />
-
-                        <input type="submit" value="Wyślij mnie"></input>
-
-                    </form>
-                )}
-            />
+                </form>
+            )}
+        />
         );
     }
 }
