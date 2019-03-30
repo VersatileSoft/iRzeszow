08dce842cbba0f9eacced720decedf25df4c3c98
LAPTOP-C4M3GNA0\Tomasz <tomkowa64@gmail.com>
2019-03-30 03:04:33 +0100

    Designing Sign in

--- src/components/signInComponent.js
+++ src/components/signInComponent.js
@@ -4,7 +4,8 @@ import { Form, Field } from 'react-final-form';
 import { updateLogInData } from '../actions/signInActions/updateLogInData';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-
+import '../styles/UniversalForm.scss';
+import Logo from '../images/logo_transparent.png';
 
 class SignIn extends Component {
 
@@ -24,29 +25,41 @@ class SignIn extends Component {
 
     render(){
         return(
-            <Form 
-            onSubmit={this.submitForm}
-            validate={this.validateForm}
-            render={({ handleSubmit, pristine, invalid }) => (
-                <form onSubmit={handleSubmit}>
-                    <Field
-                        name="email"
-                        placeholder="Email"
-                        component="input"
-                        onInput={ e => {this.handleChange('email', e.target.value)}}
-                    />
+            <div className="all">
+                <div className="form-box">
+                    <img src={Logo} alt="logo" />
+                    <p>Zaloguj</p>
+                    <p>się</p>
+                        <Form 
+                        onSubmit={this.submitForm}
+                        validate={this.validateForm}
+                        render={({ handleSubmit, pristine, invalid }) => (
+                            <div className="form">
+                                <form onSubmit={handleSubmit}>
+                                <div class="input-wrapper">
+                                        <Field
+                                            name="email"
+                                            placeholder="Email"
+                                            component="input"
+                                            onInput={ e => {this.handleChange('email', e.target.value)}}
+                                        />
+                                    </div>
+                                    <div class="input-wrapper">
+                                        <Field
+                                            name="password"
+                                            placeholder="Hasło"
+                                            component="input"
+                                            onInput={ e => {this.handleChange('password', e.target.value)}}
+                                        />
+                                    </div>
 
-                    <Field
-                        name="password"
-                        placeholder="Hasło"
-                        component="input"
-                        onInput={ e => {this.handleChange('password', e.target.value)}}
+                                    <button type="submit">Zatwierdź</button>
+                                </form>
+                            </div>
+                        )}
                     />
-
-                    <input type="submit" value="Wyślij mnie"></input>
-                </form>
-            )}
-        />
+                </div>
+        </div>
         )
     }
 }
