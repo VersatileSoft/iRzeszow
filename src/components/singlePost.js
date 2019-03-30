a967b03722ce5839d8dcc04ccb90a6795e32a52c
Marek K <wiedzmin1371@gmail.com>
2019-03-30 13:21:30 +0100

    Add posts

--- src/components/singlePost.js
+++ src/components/singlePost.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import '../styles/single-post.scss';
 
 export default class SinglePost extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state.data = props.data;
+  }
+
   render() {
     return (
-      <div className="SinglePost">
-        <div className="title">Tytuł spotkania</div>
+      <div className="SinglePost" style={"background: url(" + this.state.data.image + ") no-repeat fixed center"}>
+        <div className="title">{this.state.data.title}</div>
         <div className="continue">
           <button>Czytaj dalej</button>
         </div>
-        <div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div className="description">{this.state.data.description}</div>
       </div>
     )
   }
