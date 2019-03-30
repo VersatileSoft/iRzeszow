0b5b91055c1ec97a0897224ae0f4ff16346175c0
Konrad Kolasa <rengar600@interia.pl>
2019-03-30 10:22:34 +0100

    singlePostComponent creation start

---
+++ src/components/singlePost.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class singlePost extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Tytuł spotkania</h1>
+        <p>Przeykładowy opis, tekścik tekścik tekścik :D</p>
+      </div>
+    )
+  }
+}
